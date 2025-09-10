9daf04b045f1dacddd4304db726388114bee17e5
Eugenio <eugenio.aragon@gmail.com>
2025-09-10 17:26:50 -0300

    falta form

---
+++ Landing Page/js/site.js
@@ -0,0 +1,5 @@
+// window.addEventListener("scroll", function() {
+//   let scrollY = window.scrollY;
+//   let parallax = document.querySelector(".parallax-bg");
+//   parallax.style.transform = `translateY(${scrollY * -1.1}px)`;
+// });
