56a9953c46049232354f59093cc6857a5324fba8
= <=>
2025-09-11 09:22:15 -0300

    corregi datos contacto

--- Landing Page/js/site.js
+++ Landing Page/js/site.js
@@ -1,5 +1,12 @@
-// window.addEventListener("scroll", function() {
-//   let scrollY = window.scrollY;
-//   let parallax = document.querySelector(".parallax-bg");
-//   parallax.style.transform = `translateY(${scrollY * -1.1}px)`;
-// });
+window.addEventListener("scroll", function() {
+    let scrollY = window.scrollY;
+  
+    // Fondo parallax
+    let parallax = document.querySelector(".parallax-bg");
+    parallax.style.transform = `translateY(${scrollY * -0.7}px)`; // más suave
+  
+    // Separador
+    let separador = document.querySelector(".separador");
+    separador.style.transform = `translateY(${scrollY * -0.01}px)`; // lo podés ajustar
+  });
+  
